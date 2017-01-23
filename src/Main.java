6190cb01a3a333baba717c0599714cf68c2c6516
oksanabuday <oksanabuday@gmail.com>
2017-01-23 22:49:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Oksana on 04.01.2017.
+ */
+import static javax.print.attribute.standard.MediaSizeName.D;
+public class Main {
+    public static void main(String[] args) {
+    D a = new D ();
+    a.One( 3, 4);
+    a.Two( 2,5);
+    }
+    }
+
